dd5a977a6dde735dd14d93523984bdf41940d922
mdawaina <mohameddawaina@gmail.com>
2018-06-24 14:09:12 +0300

    End of section 4

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -14,7 +14,7 @@ class EventForm extends Component {
   }
   onFormSubmit = (evt) => {
     evt.preventDefault();
-    console.log(this.state.event);
+    this.props.createEvent(this.state.event);
   }
 
   onInputChange = (evt) => {
@@ -49,11 +49,11 @@ class EventForm extends Component {
             </Form.Field>
             <Form.Field>
               <label>Venue</label>
-              <input name='venue' onChange={this.onInputChange} value={event.title} placeholder="Enter the Venue of the event" />
+              <input name='venue' onChange={this.onInputChange} value={event.venue} placeholder="Enter the Venue of the event" />
             </Form.Field>
             <Form.Field>
               <label>Hosted By</label>
-              <input placeholder="Enter the name of person hosting" />
+              <input name='hostedBy' onChange={this.onInputChange} value={event.hostedBy} placeholder="Enter the name of person hosting" />
             </Form.Field>
             <Button positive type="submit" onClick={this.onFormSubmit}>
               Submit
